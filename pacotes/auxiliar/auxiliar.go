db441cc4e727e143217abd1b818d75528e20e541
gabriel992 <awsgabriel992@gmail.com>
2021-02-04 21:15:00 -0300

    aula pacotes

---
+++ pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+// Prind mensagem na tela
+func Escrever() {
+	fmt.Println("Escrevendo do arquivo auxiliar")
+	escrever2()
+}
---
+++ pacotes/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+// Escreve uma mensagem na tela
+func main() {
+	fmt.Println("Escrevendo do arquivo main")
+	auxiliar.Escrever()
+
+}
