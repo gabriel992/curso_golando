98fe980ed7101135f698aa6119f25469739b1ed7
gabriel992 <awsgabriel992@gmail.com>
2021-02-05 18:29:20 -0300

    variaveis

---
+++ variaveis/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var variavel1 string = "variavel1"
+	variavel2 := "variavel2"
+	fmt.Println(variavel2)
+
+	var (
+		variavel3 string = "ok"
+		variavel4 string = "ok1"
+	)
+	fmt.Println(variavel3, variavel4)
+}
